1f965c8103289b772233f24d46c4f5b362a62fda
mahmoud El3bady <113212468+MahmoudEl3bady@users.noreply.github.com>
2024-02-08 22:33:51 +0200

    Time: 47 ms (85.52%), Space: 48.7 MB (18.84%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,20 @@
+const expect = function(val) {
+    return {
+      
+        toBe: function(otherVal) {
+            if (val === otherVal) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+   
+        notToBe: function(otherVal) {
+            if (val !== otherVal) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
