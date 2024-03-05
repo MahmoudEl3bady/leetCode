ec97765924d1d0768c9d37cd296ffae0daf92e45
mahmoud El3bady <113212468+MahmoudEl3bady@users.noreply.github.com>
2024-03-05 22:20:28 +0200

    Time: 125 ms (67.68%), Space: 66.7 MB (20.69%) - LeetHub

---
+++ 2724-sort-by/2724-sort-by.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+const sortBy = function(arr, fn) {
+    return arr.sort((a,b)=>{
+        return fn(a) - fn(b);
+    })
+};
