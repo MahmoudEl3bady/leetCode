1562be579b43d7ed7abd13831b5148f4f8aa26aa
Mahmoud Elabady <113212468+MahmoudEl3bady@users.noreply.github.com>
2024-05-18 04:20:41 +0300

    Time: 69 ms (84.83%), Space: 54.3 MB (43.25%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+const moveZeroes = function (nums) {
+  let l = 0;
+  for (let r = 0; r < nums.length; r++) {
+    if (nums[r] !== 0) {
+      [nums[r], nums[l]] = [nums[l], nums[r]];
+      l++;
+    }
+  }
+  return nums;
+};
