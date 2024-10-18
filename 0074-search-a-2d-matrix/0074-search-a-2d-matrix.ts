a01fd18133aad8039bbe23fbc519d799f92fe654
Mahmoud Elabady <113212468+MahmoudEl3bady@users.noreply.github.com>
2024-10-18 19:40:16 +0300

    Time: 0 ms (100%), Space: 51.3 MB (81.89%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.ts
@@ -0,0 +1,8 @@
+function searchMatrix(matrix: number[][], target: number): boolean {
+    for(const row of matrix){
+        if(row.includes(target)){
+            return true;
+        }
+    }
+    return false;
+};
