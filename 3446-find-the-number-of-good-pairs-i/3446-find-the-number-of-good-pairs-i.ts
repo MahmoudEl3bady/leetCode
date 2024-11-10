79564c0fafd1725ef76ef13e731e358b43c5507c
Mahmoud Elabady <113212468+MahmoudEl3bady@users.noreply.github.com>
2024-11-10 21:42:59 +0200

    Time: 0 ms (100%), Space: 51.4 MB (97.5%) - LeetHub

---
+++ 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.ts
@@ -0,0 +1,12 @@
+function numberOfPairs(nums1: number[], nums2: number[], k: number): number {
+    let goodPairs = 0;
+    for(let i = 0 ;i<nums1.length ;i++ ){
+        for(let j = 0;j<nums2.length;j++){
+            if(nums1[i] % (nums2[j] * k) ==0 ){
+                goodPairs++;
+            }
+        }
+    }
+
+    return goodPairs;
+};
