d79e6d2b59a295c429b850c9b720b98acb42fa81
Mahmoud Elabady <113212468+MahmoudEl3bady@users.noreply.github.com>
2025-07-04 19:02:51 +0300

    Time: 60 ms (66.36%), Space: 56.6 MB (11.84%) - LeetHub

---
+++ 2807-execute-asynchronous-functions-in-parallel/2807-execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,25 @@
+async function promiseAll<T>(functions: (() => Promise<T>)[]): Promise<T[]> {
+  return new Promise<T[]>((resolve, reject) => {
+    if(functions.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    const res: T[] = new Array(functions.length).fill(null);
+
+    let resolvedCount = 0;
+
+    functions.forEach(async (el, idx) => {
+      try {
+        const subResult = await el();
+        res[idx] = subResult;
+        resolvedCount++;
+        if(resolvedCount === functions.length) {
+          resolve(res);
+        }
+      } catch(err) {
+        reject(err);
+      }
+    });
+  });
+};
