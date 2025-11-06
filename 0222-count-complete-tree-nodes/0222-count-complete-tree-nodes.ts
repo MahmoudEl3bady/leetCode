a688e90ec0a400c3fa19222ed7a5c6b483b418a7
Mahmoud Elabady <113212468+MahmoudEl3bady@users.noreply.github.com>
2025-11-06 21:41:49 +0200

    Time: 0 ms (100%), Space: 74.9 MB (62.19%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+    let count = 0;
+    const dfs = (root) => {
+        if (!root) return 0;
+        count++;
+        if (root.left) dfs(root.left);
+        if (root.right) dfs(root.right);
+        return count;
+    }
+
+    return dfs(root);
+};
